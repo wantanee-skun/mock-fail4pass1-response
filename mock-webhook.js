99133b808861137bcfce8102a36688a6e0f16b10
wantanee-sku <wantanee.skun@gmail.com>
2025-07-24 09:59:50 +0700

    Update mock-webhook.js

--- mock-webhook.js
+++ mock-webhook.js
@@ -10,12 +10,16 @@ app.use(express.json());
 app.post('/webhook', (req, res) => {
   requestCount++;
 
+ // Log full request body
+  console.log(`\n=== Attempt ${requestCount} ===`);
+  console.log("Request Body:", JSON.stringify(req.body, null, 2));
+
   const cycle = requestCount % 5;
-  if (cycle === 1 || cycle === 2 || cycle === 3 || cycle=== 4 ) {
-    console.log(`Attempt ${requestCount}: Returning 400`);
+  if (cycle === 1 || cycle === 2 || cycle === 3 || cycle === 5) {
+    console.log("Returning HTTP 400");
     return res.status(400).json({ error: 'Mock 400 error' });
   } else {
-    console.log(`Attempt ${requestCount}: Returning 200`);
+    console.log("Returning HTTP 200");
     return res.status(200).json({ message: 'Success' });
   }
 });
