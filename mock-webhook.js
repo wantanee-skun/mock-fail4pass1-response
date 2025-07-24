7bc3f5412a85a4149d7f8da9fe02412633639b0a
wantanee-sku <wantanee.skun@gmail.com>
2025-07-24 10:02:24 +0700

    Update mock-webhook.js

--- mock-webhook.js
+++ mock-webhook.js
@@ -15,7 +15,7 @@ app.post('/webhook', (req, res) => {
   console.log("Request Body:", JSON.stringify(req.body, null, 2));
 
   const cycle = requestCount % 5;
-  if (cycle === 1 || cycle === 2 || cycle === 3 || cycle === 5) {
+  if (cycle === 1 || cycle === 2 || cycle === 3 || cycle === 4) {
     console.log("Returning HTTP 400");
     return res.status(400).json({ error: 'Mock 400 error' });
   } else {
